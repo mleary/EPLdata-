96c2f0ce602d24b44970cb5d35a4ed9342e7753d
matt l <33221468+mleary@users.noreply.github.com>
2018-12-21 09:43:41 -0500

    Updated chart

--- 181220_PochettinoEffect.R
+++ 181220_PochettinoEffect.R
@@ -1,21 +1,28 @@
+# By: Matt Leary
+# Date: Dec 20, 2018
+# Intent: Look at THFC perforamnce, comparted to other London based teams,
+#        since Pochettino installed as manager
+# Note: Inspiration for project taken from John Burn-Murdoch TWitter: @jburnmurdoch####
+# From https://gist.github.com/johnburnmurdoch/1b3f32aaf7757733bd68a6513ab86226 
 
 
 library(tidyverse)
 library(ggthemes)
 
 ## Load Team data
-poch_start <- as.Date('2014-07-01')
+poch_start <- as.Date('2014-05-27')
+current_date <- today()
 
 tottenham <- read_csv("http://api.clubelo.com/tottenham") %>% 
-  filter(From >= poch_start) 
+  filter(From >= poch_start, To <= current_date) 
 chelsea <- read_csv("http://api.clubelo.com/chelsea") %>% 
-  filter(From >= poch_start)
+  filter(From >= poch_start, To <= current_date) 
 arsenal <- read_csv("http://api.clubelo.com/arsenal") %>% 
-  filter(From >= poch_start)
-westham <-read_csv("http://api.clubelo.com/westham") %>% 
-  filter(From >= poch_start) 
-palace <- read_csv("http://api.clubelo.com/crystalpalace") %>% 
-  filter(From >= poch_start) 
+  filter(From >= poch_start, To <= current_date) 
+# westham <-read_csv("http://api.clubelo.com/westham") %>% 
+#   filter(From >= poch_start, To <= current_date) 
+# palace <- read_csv("http://api.clubelo.com/crystalpalace") %>% 
+#   filter(From >= poch_start, To <= current_date) 
 
 ## Assign  Some Variables
 season1 <- as.Date('2014-08-16')
@@ -26,10 +33,14 @@ season5 <- as.Date('2018-08-11')
 
 seasons <- c(season1, season2, season3, season4,season5)
 
-###Order Alphabetically
-club_colors <- c("#ef0107", "#DBA111","#1B458F", "#132257", "#7A263A")
+###Order Alphabetically ()
+#club_colors <- c("#ef0107", "#DBA111","#1B458F", "#132257", "#7A263A")
+club_colors <- c("#ef0107", "#DBA111", "#132257")
 
-combined_teams <- bind_rows(arsenal, chelsea, palace, tottenham, westham)
+
+#combined_teams <- bind_rows(arsenal, chelsea, palace, tottenham, westham)
+combined_teams <- bind_rows(arsenal, chelsea, tottenham)
+
 
 start_labels <-combined_teams[!duplicated(combined_teams$Club),] %>% 
   mutate(x_position = as.Date("2014-05-01"))
@@ -44,53 +55,17 @@ end_labels <- end_labels[!duplicated(end_labels$Club),] %>%
 ## Plot
 ggplot(combined_teams, aes(x = To, y = Elo, color = Club)) +
   geom_line(size = 1.5) +
+  geom_text(data = start_labels, aes(x = x_position, y = Elo, label = Club, size = 2.75)) +
+  geom_text(data = end_labels, aes(x = x_position, y = Elo, label = Club, size =  2.75)) +
   scale_color_manual(values = club_colors) +
   scale_x_date(breaks=seasons,  date_labels="%Y-%B") +
-  geom_text(data = start_labels, aes(x = x_position, y = Elo, label = Club)) +
-  geom_text(data = end_labels, aes(x = x_position, y = Elo, label = Club)) +
   labs(y = NULL,
        x = NULL,
-       title = "The Pochettino Effect: Tottenham takes over London following Mauricio Pochettino's arrival",
-       subtitle = "Club performance based on Elo rating system (clubElo.com)",
-       caption = "Concept based on work of @jburnmurdoch")+ 
-  theme_fivethirtyeight()
+       title = "The Pochettino Effect: Tottenham's performance since Mauricio Pochettino's arrival in May, 2014",
+       subtitle = "Club performance based on Elo rating system (clubelo.com)") + 
+  theme_fivethirtyeight() +
+  theme(legend.position = "none")
 
 
   
  
-###Inspiration for project taken from John Burn-Murdoch TWitter: @jburnmurdoch####
- From https://gist.github.com/johnburnmurdoch/1b3f32aaf7757733bd68a6513ab86226 
-jose.porto <- read_csv("http://api.clubelo.com/porto")
-jose.chelsea <- read_csv("http://api.clubelo.com/chelsea")
-jose.inter <- read_csv("http://api.clubelo.com/inter")
-jose.real <- read_csv("http://api.clubelo.com/realmadrid")
-jose.mufc <- read_csv("http://api.clubelo.com/manunited")
-
-jose.all <- bind_rows(
-  jose.porto %>% filter(From >= as.Date("2002-01-22") & To <= as.Date("2004-06-30")),
-  jose.chelsea %>% 
-    filter(
-      (From >= as.Date("2004-07-01") & To <= as.Date("2007-09-19"))
-    ),
-  jose.chelsea %>% 
-    filter(
-      (From >= as.Date("2013-07-01") & To <= as.Date("2015-12-17"))
-    ) %>% mutate(Club = "Chelsea 2"),
-  jose.inter %>% filter(From >= as.Date("2008-07-01") & To <= as.Date("2010-06-30")),
-  jose.real %>% filter(From >= as.Date("2010-07-01") & To <= as.Date("2013-06-30")),
-  jose.mufc %>% filter(From >= as.Date("2016-07-01") & To <= as.Date("2018-12-18"))
-)
-
-jose.labels <- jose.all %>%
-  group_by(Club) %>%
-  top_n(1, Elo)
-
-ggplot(jose.all, aes(To, Elo, group=Club, color=Club)) +
-  theme_minimal() +
-  geom_line() +
-  geom_text(data=jose.labels, aes(y = Elo+10, label=Club)) +
-  scale_color_discrete(guide=F) +
-  scale_y_continuous(position = "right") +
-  scale_x_date(date_breaks="2 years", date_labels="%Y") +
-  labs(x = "", y = "", subtitle = "Team performance level (Elo rating)", title = "Mourinho’s recent stints have been marked by steep declines in performance after promising starts")
-
